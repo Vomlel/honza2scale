d3fc5b7d2501cb8538bc16185de500575f3d55c3
Vomlel <honza.vomlel@gmail.com>
2021-04-28 18:51:13 +0200

    first data in frontend!!!

--- src/frontend/src/components/HelloWorld.vue
+++ src/frontend/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
     <h1>{{msg}}</h1>
+    <h1>{{msg2}}</h1>
 </template>
 
 <script>
@@ -7,15 +8,21 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-        msg: ''
+        msg: '',
+        msg2: ''
     }
   },
   mounted() {
     fetch("/api/messages/hello")
-        .then((response) => response.text())
-        .then((data) => {
-            this.msg = data;
-        })
+            .then((response) => response.text())
+            .then((data) => {
+                this.msg = data;
+            })
+    fetch("/api/messages/user/1")
+            .then((response) => response.json())
+            .then((data) => {
+                this.msg2 = data.username;
+            })
   }
 }
 </script>
--- src/main/java/net/toscalecz/test/controller/MessageController.java
+++ src/main/java/net/toscalecz/test/controller/MessageController.java
@@ -1,14 +1,32 @@
 package net.toscalecz.test.controller;
 
+import net.toscalecz.test.rest.User;
+import net.toscalecz.test.rest.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/messages")
 public class MessageController {
+
+    private final UserRepository repository;
+
+    @Autowired
+    public MessageController(UserRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/hello")
     public String hello(){
         return "Hello Chris!";
+    }
+    @GetMapping("/user/{id}")
+    public Optional<User> helloUser(@PathVariable Long id){
+        return repository.findById(id);
     }
 }
