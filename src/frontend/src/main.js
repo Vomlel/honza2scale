e5b5a68c3752661c435d2db6b2265c88c80ebdc2
Christian Scharmach <cs@2scale.net>
2021-04-29 12:09:46 +0200

    fix: use proper router version for vuejs3

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,8 +1,7 @@
 import App from './App.vue'
+import { createApp } from 'vue'
 import router from './router'
-import Vue from 'vue'
 
-new Vue ({
-    router,
-    render: h => h(App),
-}).$mount('#app')
+createApp(App)
+.use(router)
+.mount('#app')
--- src/frontend/src/router.js
+++ src/frontend/src/router.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './components/Home.vue'
-import Login from './components/Login.vue'
+import {createRouter, createWebHashHistory} from "vue-router";
+import Home from "@/components/Home";
+import Login from "@/components/Login";
 
-Vue.use(Router)
-
-export default new Router({
-    mode: 'history',
-    routes: [
-        {path: '/', component: Home},
-        {path: '/login', component: Login}
-    ]
+const routes = [
+    {path: '/', component: Home},
+    {path: '/login', component: Login}
+]
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
 })
+
+export default router
